3fa08fdf4961eac3962d9ddda4df4a3d43e7cbd5
fixmation <webprojects@onmail.com>
2025-06-10 19:39:02 +0530

    Add WordPressAutomationTool/static/js/language.js 1749564541956

---
+++ WordPressAutomationTool/static/js/language.js
@@ -0,0 +1,6 @@
+// Translation functionality temporarily disabled
+/*
+document.addEventListener('DOMContentLoaded', async () => {
+    // Translation logic commented out - to be implemented in the future
+});
+*/
